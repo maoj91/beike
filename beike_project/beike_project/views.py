56e0f61b4924c918fef1f2ad277f1d021218356c
yurenji <yurenji@gmail.com>
2014-02-09 02:26:41 +0000

    fix minor bug

--- beike_project/beike_project/views.py
+++ beike_project/beike_project/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponseRedirect
 from data.models import User,State,City,Area,Notification,Privacy
 from django.views.decorators.csrf import csrf_exempt
 
-def index(request):
+def index(request,userid):
 	return render_to_response('index.html')
