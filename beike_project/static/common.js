cffda8ddc9ed0fe1e87f0c673a78e1363c80845f
yurenji <yurenji@gmail.com>
2014-09-08 23:29:04 -0700

    put user.js into common.js

--- beike_project/static/common.js
+++ beike_project/static/common.js
@@ -905,3 +905,89 @@ $(document).on("pagechange", function() {
     }
 });
 
+
+/******************************************
+**    User info/edit                
+******************************************/
+var userLoader = (function($, undefined) {
+    var loader = {},
+        page, $page;
+
+    loader.init = function(initPage) {
+        page = initPage;
+        $page = $('#user-update-page');     
+        $page.find('#description').bind('input propertychange', function() {
+            var contentLength = $(this).val().length;
+            $page.find('#lengthCounter').html(contentLength + '/3000');
+        });
+
+        //init lengthCounter
+        var contentLength = $('#description').val().length;
+        var lengthCount = contentLength + "/" + 3000;
+        $('#lengthCounter').text(lengthCount);
+
+        var isUploading = false;
+        $('#post_image').fileupload({
+            url: "/s3/upload/",
+            dataType: 'json',
+            done: function(e, data) {
+                var imageInfo = {};
+                imageInfo['url'] = data.result.image_url;
+                imageInfo['width'] = data.result.width;
+                imageInfo['height'] = data.result.height;
+                imageInfo['orientation'] = data.result.orientation;
+                $('#image_url').val(imageInfo['url']);
+                $('#image_width').val(imageInfo['width']);
+                $('#image_height').val(imageInfo['height']);
+                $('#image_orientation').val(imageInfo['orientation']);
+                $('#profile_image').attr('src',imageInfo['url']);
+                console.log(JSON.stringify(imageInfo));
+            },
+            progressall: function(e, data) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                $('#progressbar').show();
+                jQMProgressBar('progressbar').setValue(progress);
+                isUploading = true;
+                $('#image-uploader').attr("disabled", true);
+                $('#post_image').attr("disabled", true);
+            },
+            fail: function(e, data) {
+                alert("照片上传出错，请重试一次");
+            },
+            always: function(e, data) {
+                $('#progressbar').hide();
+                isUploading = false;
+                $('#image-uploader').attr("disabled", false);
+                $('#post_image').attr("disabled", false);
+            }
+        });
+    };
+
+    return loader;
+}(jQuery));
+
+$(document).delegate("#user-update-page", "pageinit", function() {
+    userLoader.init('user-update-page');
+});
+
+
+/******************************************
+**    Address edit
+******************************************/
+var addressLoader = (function($, undefined) {
+    var loader = {},
+        page, $page, 
+        $latitude, $longitude,
+        $zipcode, $cityName;
+
+    loader.init = function(initPage) {
+        page = initPage;
+        $page = $('#user-address-page');     
+    };
+
+    return loader;
+}(jQuery));
+
+$(document).delegate("#user-address-page", "pageinit", function() {
+    addressLoader.init('user-address-page');
+});
