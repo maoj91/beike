6d0b1ef4437f6835c82723ea35b8e4a54219176d
yurenji <yurenji@gmail.com>
2014-09-10 22:27:03 -0700

    move js of user_address_edit and get_info to common.js

--- beike_project/static/common.js
+++ beike_project/static/common.js
@@ -981,13 +981,116 @@ var addressLoader = (function($, undefined) {
         $zipcode, $cityName;
 
     loader.init = function(initPage) {
-        page = initPage;
-        $page = $('#user-address-page');     
+        $page = $('#'+initPage); 
+        if (navigator.geolocation) {
+            getCurrentPositionDeferred({
+                enableHighAccuracy: true
+            }).done(function(position) {
+                loader.getLocationByLatLon(position.coords)
+            }).fail(function() {
+                console.log("getCurrentPosition call failed")
+            }).always(function() {
+                //do nothing
+            });
+        } else {
+            console.error("Geolocation is disabled.")
+        }
+
+        $(document).on("click", "#getzipcode", function() {
+            var zipcode = $('#zipcode_input').val();
+            if (zipcode) {
+                $.ajax({
+                    type: "get",
+                    url: "/user/get_info/get_city",
+                    dataType: "json",
+                    data: {
+                        zipcode: zipcode,
+                    }
+                }).then(function(data) {
+                    console.log(data);
+                    $("#city").text(data.city_name);
+                    $("#city_id").val(data.city_id);
+                    $("#district").text(data.lv1_district_name);
+                    $("#latitude").val(data.latitude);
+                    $("#longitude").val(data.longitude);
+                });
+            }
+        });
+
     };
+
+     loader.getLocationByLatLon = function(coords) {
+            var latitude = coords.latitude;
+            var longitude = coords.longitude;
+            $.ajax({
+                type: "get",
+                url: "/user/get_info/get_city_by_latlong",
+                dataType: "json",
+                data: {
+                    latitude: latitude,
+                    longitude: longitude
+                }
+            }).then(function(data) {
+                $("#city").text(data.city_name);
+                $("#city_id").val(data.city_id);
+                $("#district").text(data.lv1_district_name);
+                $("#zipcode").val(data.zipcode);
+                $("#latitude").val(latitude);
+                $("#longitude").val(longitude);
+            });
+    }
 
     return loader;
 }(jQuery));
 
 $(document).delegate("#user-address-page", "pageinit", function() {
     addressLoader.init('user-address-page');
+});
+
+
+/******************************************
+**    user getinfo 
+******************************************/
+var infoLoader = (function($, undefined) {
+    var loader = {}, $page;
+
+    loader.init = function(initPage) {
+        $page = $('#'+initPage);
+
+    };
+
+    return loader;
+}(jQuery));
+
+function validateUserInfo() {
+            $('#user_info_form').validate({
+                rules: {
+                    user_email: {
+                        required: true,
+                        email: true,
+                        remote: {
+                            url: "check_email",
+                            type: "post",
+                            async: false
+                        }
+                    }
+                },
+                messages: {
+                    user_email: {
+                        email: "该email格式不对",
+                        remote: "该email已经被使用"
+                    }
+                }
+            });
+            $('#username_input').val($('#username').val());
+            $('#email_input').val($('#user_email').val());
+            return $('#user_info_form').valid();
+}
+
+$(document).delegate("#user_info", "pageinit", function() {
+    infoLoader.init('user_info');
+});
+
+$(document).delegate("#user_location", "pageinit", function() {
+    addressLoader.init('user_location');
 });
